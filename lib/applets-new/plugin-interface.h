d38a476772431ad69a80f2a80d4375e628e5a8da
Konstantin Pugin <ria.freelander@gmail.com>
2016-08-06 13:30:53 +0300

    Init C Beautifuller

--- lib/applets-new/plugin-interface.h
+++ lib/applets-new/plugin-interface.h
@@ -1,21 +1,22 @@
 #ifndef APPLETWIDGET_H
 #define APPLETWIDGET_H
 
-#include <glib.h>
-#include <glib-object.h>
 #include "applet-widget.h"
+#include <glib-object.h>
+#include <glib.h>
 
 G_BEGIN_DECLS
-G_DECLARE_INTERFACE(ValaPanelPluginInterface,vala_panel_plugin_interface,VALA_PANEL,PUGIN_INTERFACE,GObject);
+G_DECLARE_INTERFACE(ValaPanelPluginInterface, vala_panel_plugin_interface,
+                    VALA_PANEL, PUGIN_INTERFACE, GObject);
 
 struct _ValaPanelPluginInterface
 {
-    GTypeInterface parent_iface;
-    ValaPanelAppletWidget* (*get_applet_widget) (ValaPanelPluginInterface* self,
-                                                 const gchar* uuid,
-                                                 const gchar* name,
-                                                 const gchar* path,
-                                                 const gchar* filename);
+  GTypeInterface parent_iface;
+  ValaPanelAppletWidget* (*get_applet_widget)(ValaPanelPluginInterface* self,
+                                              const gchar* uuid,
+                                              const gchar* name,
+                                              const gchar* path,
+                                              const gchar* filename);
 };
 G_END_DECLS
 
