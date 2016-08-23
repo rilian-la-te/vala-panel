e0a190cf690a3db737a79aa9b4e90775b5b580fd
Konstantin <ria.freelander@gmail.com>
2016-08-23 12:00:07 +0300

    Enum changes + more iface work

--- lib/c-lib/panel-position-iface.c
+++ lib/c-lib/panel-position-iface.c
@@ -1,1 +1,8 @@
 #include "panel-position-iface.h"
+
+G_DEFINE_INTERFACE(ValaPanelPositionIface,vala_panel_position_iface,G_TYPE_OBJECT)
+
+void vala_panel_position_iface_default_init(ValaPanelPositionIfaceInterface* self)
+{
+
+}
--- lib/c-lib/panel-position-iface.h
+++ lib/c-lib/panel-position-iface.h
@@ -2,16 +2,30 @@
 #define PANELPOSITIONIFACE_H
 
 #include <glib-object.h>
+#include <stdbool.h>
 #include "toplevel.h"
 
 G_BEGIN_DECLS
 
 G_DECLARE_INTERFACE(ValaPanelPositionIface,vala_panel_position_iface,VALA_PANEL,POSITION_IFACE,GObject)
 
+typedef enum
+{
+    AH_HIDDEN,
+    AH_WAITING,
+    AH_VISIBLE
+} AutohideState;
+
 struct _ValaPanelPositionIfaceInterface
 {
     GTypeInterface g_iface;
-    void (*update_strut) (ValaPanelToplevelUnit* top);
+    /*struts*/
+    bool (*can_strut) (ValaPanelPositionIface* f, ValaPanelToplevelUnit* top);
+    void (*update_strut) (ValaPanelPositionIface* f, ValaPanelToplevelUnit* top);
+    /*autohide*/
+    bool (*ah_start)(ValaPanelPositionIface* f,ValaPanelToplevelUnit* top);
+    bool (*ah_stop)(ValaPanelPositionIface* f,ValaPanelToplevelUnit* top);
+    bool (*ah_state_set)(ValaPanelPositionIface* f,ValaPanelToplevelUnit* top);
     gpointer padding [12];
 };
 
