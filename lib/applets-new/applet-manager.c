5e01dc08bd95585fcd452f10be702900e46cf32d
Konstantin <ria.freelander@gmail.com>
2016-08-26 09:34:50 +0300

    Add AppletManager singleton. I must write it now.

---
+++ lib/applets-new/applet-manager.c
@@ -0,0 +1,20 @@
+#include "applet-manager.h"
+
+struct _ValaPanelAppletManager
+{
+
+};
+
+G_DEFINE_TYPE(ValaPanelAppletManager,vala_panel_applet_manager,G_TYPE_OBJECT)
+
+
+
+void vala_panel_applet_manager_init(ValaPanelAppletManager* self)
+{
+
+}
+
+void vala_panel_applet_manager_class_init(ValaPanelAppletManagerClass* klass)
+{
+
+}
---
+++ lib/applets-new/applet-manager.h
@@ -0,0 +1,12 @@
+#ifndef APPLETMANAGER_C
+#define APPLETMANAGER_C
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+G_DECLARE_FINAL_TYPE(ValaPanelAppletManager,vala_panel_applet_manager,VALA_PANEL,APPLET_MANAGER,GObject)
+
+G_END_DECLS
+
+#endif // APPLETMANAGER_C
