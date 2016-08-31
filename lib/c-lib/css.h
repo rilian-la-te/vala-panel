56b7321369505f311450c14142b1aaa015c98b7a
Konstantin Pugin <ria.freelander@gmail.com>
2016-08-31 23:45:34 +0300

    Modify to match exiting vala-panel behavoir.

--- lib/c-lib/css.h
+++ lib/c-lib/css.h
@@ -14,6 +14,6 @@ char *css_generate_font_label(gfloat size, bool is_bold);
 char *css_apply_from_file(GtkWidget *widget, const char *file);
 char *css_apply_from_resource(GtkWidget *widget, const char *file, const char *klass);
 char *css_apply_from_file_to_app(const char *file);
-// inline char* css_generate_flat_button(GtkWidget* widget,ValaPanel* panel);
+char* css_generate_flat_button(GtkWidget* widget,GtkPositionType edge);
 
 #endif
