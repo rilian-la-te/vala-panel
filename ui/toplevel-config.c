6a7a07236eb56b927a89b7cb9e80bc40a59552cb
Konstantin <ria.freelander@gmail.com>
2018-05-27 15:07:35 +0300

    configurator: reimplement on_add_plugin.

--- ui/toplevel-config.c
+++ ui/toplevel-config.c
@@ -405,12 +405,11 @@ static void init_plugin_list(ValaPanelToplevelConfig *self)
 {
 	GtkTreeIter it;
 	GtkCellRenderer *textrender = gtk_cell_renderer_pixbuf_new();
-	GtkTreeViewColumn *col =
-	    gtk_tree_view_column_new_with_attributes(_("Currently loaded plugins"),
-	                                             textrender,
-	                                             "icon-name",
-	                                             COLUMN_ICON,
-	                                             NULL);
+	GtkTreeViewColumn *col      = gtk_tree_view_column_new_with_attributes(_("Icon"),
+                                                                          textrender,
+                                                                          "icon-name",
+                                                                          COLUMN_ICON,
+                                                                          NULL);
 	gtk_tree_view_column_set_expand(col, true);
 	gtk_tree_view_append_column(self->plugin_list, col);
 	g_object_unref(col);
@@ -449,6 +448,128 @@ static void init_plugin_list(ValaPanelToplevelConfig *self)
 	g_signal_connect(tree_sel, "changed", G_CALLBACK(on_sel_plugin_changed), self);
 	if (gtk_tree_model_get_iter_first(list, &it))
 		gtk_tree_selection_select_iter(tree_sel, &it);
+}
+
+static void on_configure_plugin(GtkButton *btn, void *data)
+{
+	ValaPanelToplevelConfig *self = VALA_PANEL_TOPLEVEL_CONFIG(data);
+	GtkTreeSelection *tree_sel    = gtk_tree_view_get_selection(self->plugin_list);
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	ValaPanelApplet *pl;
+	if (!gtk_tree_selection_get_selected(tree_sel, &model, &iter))
+		return;
+	gtk_tree_model_get(model, &iter, COLUMN_DATA, &pl, -1);
+	if (vala_panel_applet_is_configurable(pl))
+		vala_panel_applet_show_config_dialog(pl);
+}
+static int sort_by_name(GtkTreeModel *model, GtkTreeIter *a, GtkTreeIter *b, void *unused)
+{
+	char *str_a, *str_b;
+	gtk_tree_model_get(model, a, 0, &str_a, -1);
+    gtk_tree_model_get(model, b, 0, &str_b, -1);
+	int ret = g_utf8_collate(str_a, str_b);
+	g_free(str_a);
+	g_free(str_b);
+	return ret;
+}
+
+static void update_widget_position_keys(ValaPanelToplevelConfig *self)
+{
+	ValaPanelLayout *layout       = vala_panel_toplevel_get_layout(self->_toplevel);
+	g_autoptr(GList) applets_list = vala_panel_layout_get_applets_list(layout);
+	for (GList *l = applets_list; l != NULL; l = g_list_next(l))
+	{
+		ValaPanelApplet *applet  = VALA_PANEL_APPLET(l->data);
+		uint idx                 = vala_panel_layout_get_applet_position(layout, applet);
+		ValaPanelUnitSettings *s = vala_panel_layout_get_applet_settings(layout, applet);
+		g_settings_set_uint(s->default_settings, VALA_PANEL_KEY_POSITION, idx);
+	}
+}
+
+static void on_add_plugin_row_activated(GtkTreeView *tree_view, GtkTreePath *path,
+                                        GtkTreeViewColumn *column, gpointer user_data)
+{
+	ValaPanelToplevelConfig *self = VALA_PANEL_TOPLEVEL_CONFIG(user_data);
+	GtkTreeIter it;
+	GtkTreeModel *model;
+	GtkTreeSelection *sel = gtk_tree_view_get_selection(tree_view);
+	if (gtk_tree_selection_get_selected(sel, &model, &it))
+	{
+		g_autofree char *type;
+		gtk_tree_model_get(model, &it, 1, &type, -1);
+		ValaPanelLayout *layout = vala_panel_toplevel_get_layout(self->_toplevel);
+		vala_panel_layout_add_applet(layout, type);
+		vala_panel_layout_update_applet_positions(layout);
+		update_plugin_list_model(self);
+	}
+	update_widget_position_keys(self);
+}
+
+static void on_add_plugin(GtkButton *btn, ValaPanelToplevelConfig *self)
+{
+	GtkPopover *dlg           = gtk_popover_new(self->adding_button);
+	GtkScrolledWindow *scroll = gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_shadow_type(scroll, GTK_SHADOW_IN);
+	gtk_scrolled_window_set_policy(scroll, GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_container_add(dlg, scroll);
+	GtkTreeView *view = gtk_tree_view_new();
+	gtk_container_add(scroll, view);
+	GtkTreeSelection *tree_sel = gtk_tree_view_get_selection(view);
+	gtk_tree_selection_set_mode(tree_sel, GTK_SELECTION_BROWSE);
+
+	GtkCellRenderer *render = gtk_cell_renderer_pixbuf_new();
+	GtkTreeViewColumn *col  = gtk_tree_view_column_new_with_attributes(_("Icon"),
+                                                                          render,
+                                                                          "icon-name",
+                                                                          COLUMN_ICON,
+                                                                          NULL);
+
+	gtk_tree_view_append_column(view, col);
+	g_object_unref(render);
+	g_object_unref(col);
+	render = gtk_cell_renderer_text_new();
+	col    = gtk_tree_view_column_new_with_attributes(_("Available plugins"),
+                                                       render,
+                                                       "text",
+                                                       1,
+                                                       NULL);
+	gtk_tree_view_append_column(view, col);
+	g_object_unref(render);
+	g_object_unref(col);
+
+	GtkListStore *list = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+
+	/* Populate list of available plugins.
+	 * Omit plugins that can only exist once per system if it is already configured. */
+	g_autoptr(GList) all_types =
+	    vala_panel_applet_holder_get_all_types(vala_panel_layout_holder);
+	for (GList *l = all_types; l != NULL; l = g_list_next(l))
+	{
+		const char *once =
+		    peas_plugin_info_get_external_data(l->data, VALA_PANEL_DATA_ONE_PER_SYSTEM);
+		if (once == NULL || !peas_plugin_info_is_loaded(l->data))
+		{
+			GtkTreeIter it;
+			gtk_list_store_append(list, &it);
+			/* it is safe to put classes data here - they will be valid until restart */
+			const char *icon_name   = peas_plugin_info_get_icon_name(l->data);
+			const char *name        = _(peas_plugin_info_get_name(l->data));
+			const char *module_name = peas_plugin_info_get_module_name(l->data);
+
+			gtk_list_store_set(list, &it, 0, icon_name, 1, name, 2, module_name, -1);
+		}
+	}
+	gtk_tree_sortable_set_default_sort_func(list, sort_by_name, self, NULL);
+	gtk_tree_sortable_set_sort_column_id(list,
+	                                     GTK_TREE_SORTABLE_DEFAULT_SORT_COLUMN_ID,
+	                                     GTK_SORT_ASCENDING);
+	gtk_tree_view_set_activate_on_single_click(view, true);
+	gtk_tree_view_set_model(view, list);
+	g_signal_connect(view, "row-activated", G_CALLBACK(on_add_plugin_row_activated), self);
+	gtk_scrolled_window_set_min_content_width(scroll, 320);
+	gtk_scrolled_window_set_min_content_height(scroll, 200);
+	gtk_widget_show_all(dlg);
 }
 
 static void vala_panel_toplevel_config_class_init(ValaPanelToplevelConfigClass *klass)
@@ -556,12 +677,12 @@ static void vala_panel_toplevel_config_class_init(ValaPanelToplevelConfigClass *
 	gtk_widget_class_bind_template_callback_full(GTK_WIDGET_CLASS(klass),
 	                                             "on_monitor_changed",
 	                                             G_CALLBACK(on_monitors_changed));
-	//        gtk_widget_class_bind_template_callback_full (GTK_WIDGET_CLASS (klass),
-	//        "on_configure_plugin",
-	//        G_CALLBACK(_vala_panel_configure_dialog_on_configure_plugin_gtk_button_clicked));
-	//    gtk_widget_class_bind_template_callback_full (GTK_WIDGET_CLASS (klass),
-	//    "on_add_plugin",
-	//    G_CALLBACK(_vala_panel_configure_dialog_on_add_plugin_gtk_button_clicked));
+	gtk_widget_class_bind_template_callback_full(GTK_WIDGET_CLASS(klass),
+	                                             "on_configure_plugin",
+	                                             G_CALLBACK(on_configure_plugin));
+	gtk_widget_class_bind_template_callback_full(GTK_WIDGET_CLASS(klass),
+	                                             "on_add_plugin",
+	                                             G_CALLBACK(on_add_plugin));
 	//    gtk_widget_class_bind_template_callback_full (GTK_WIDGET_CLASS (klass),
 	//    "on_remove_plugin",
 	//    G_CALLBACK(_vala_panel_configure_dialog_on_remove_plugin_gtk_button_clicked));
