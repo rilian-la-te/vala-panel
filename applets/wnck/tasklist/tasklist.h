c252bca700d217bcb2d7712d45957f60e65cf795
Konstantin <ria.freelander@gmail.com>
2019-04-07 04:39:46 +0300

    tasklist: correct header

--- applets/wnck/tasklist/tasklist.h
+++ applets/wnck/tasklist/tasklist.h
@@ -1,5 +1,5 @@
 /*
- * generic-monitor-plugin
+ * vala-panel
  * Copyright (C) 2015-2018 Konstantin Pugin <ria.freelander@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
