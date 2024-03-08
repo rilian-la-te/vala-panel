ffdca7bf064c1be6ebf2ccb30589825ab1a6b31f
Konstantin <ria.freelander@gmail.com>
2024-03-08 21:12:32 +0300

    vala-panel: create main header
    This will be useful for GIR

---
+++ ui/vala-panel.h
@@ -0,0 +1,20 @@
+/* SPDX Li*/
+
+#ifndef VALA_PANEL_H
+#define VALA_PANEL_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#include "applet-info.h"
+#include "applet-widget-api.h"
+#include "definitions.h"
+#include "util-gtk.h"
+#include "panel-platform.h"
+#include "settings-manager.h"
+#include "toplevel.h"
+
+G_END_DECLS
+
+#endif
