bc2e2a13c41fd9ebe7e7721424736d31403462fc
Konstantin <ria.freelander@gmail.com>
2024-03-17 01:00:17 +0300

    applet-widget-api: remove untyped constructor

--- ui/applet-widget-api.h
+++ ui/applet-widget-api.h
@@ -6,8 +6,8 @@
 
 /**
  * vala_panel_applet_new_with_type: (constructor)
- * @type: type of new Applet. It should be a subclass of #ValaPanelApplet
- * @toplevel: #ValaPanelToplevel parent
+ * @ex: type of new Applet. It should be a subclass of #ValaPanelApplet
+ * @top: #ValaPanelToplevel parent
  * @settings:  #GSettings for these applet type (not generic one)
  * @uuid: UUID for these applet
  * 
@@ -15,19 +15,6 @@
  */
 gpointer vala_panel_applet_new_with_type(GType ex, ValaPanelToplevel *top, GSettings *settings,
                                      const char *uuid);
-/**
- * vala_panel_applet_new: (constructor)
- * @toplevel: #ValaPanelToplevel parent
- * @settings:  #GSettings for these applet type (not generic one)
- * @uuid: UUID for these applet
- * 
- * Returns: (nullable) (transfer full): a new #ValaPaneApplet
- */
-static inline ValaPanelApplet* vala_panel_applet_new(ValaPanelToplevel *top, GSettings *settings,
-                                     const char *uuid)
-{
-    return vala_panel_applet_new_with_type(VALA_PANEL_TYPE_APPLET, top, settings, uuid);
-}
 
 /**
  * vala_panel_applet_get_toplevel: (get-property toplevel)
